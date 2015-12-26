52d8e929fa3d69d069bd9ed4c2dbec6ad947f454
kadoppe <kadoppe@me.com>
2015-12-26 21:05:43 +0900

    Ruby削除

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'wiringpi', git: "https://github.com/WiringPi/WiringPi-Ruby-Legacy.git"
-
--- motor.rb
+++
@@ -1,9 +0,0 @@
-require 'wiringpi'
-s = WiringPi::GPIO.new
-
-s.mode(18, PWM_OUTPUT)
-s.pwmSetMode(0)
-s.pwmSetClock(400)
-s.pwmSetRange(1024)
-
-s.pwmWrite(18, 100)
