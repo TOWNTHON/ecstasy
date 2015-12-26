17d6a39c386edaccf5121fd6bfbe17f978b442eb
kadoppe <kadoppe@me.com>
2015-12-26 22:21:06 +0900

    サーボへの出力インターバルを変更

--- motor.py
+++ motor.py
@@ -39,7 +39,9 @@ while True:
     for dc in range(2, 12, 1):
       servo.ChangeDutyCycle(dc)
       print("dc = %d" % dc)
+      time.sleep(0.01)
 
     for dc in range(12, 2, -1):
       servo.ChangeDutyCycle(dc)
       print("dc = %d" % dc)
+      time.sleep(0.01)
