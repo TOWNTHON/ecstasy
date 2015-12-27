d9f3395e922f5173cebd74149034caa6e4839821
kadoppe <kadoppe@me.com>
2015-12-27 09:34:13 +0900

    各クラスにコメントを追加

--- engine/servo.py
+++ engine/servo.py
@@ -4,15 +4,19 @@
 import RPi.GPIO as GPIO
 import time
 
+# サーボ制御用クラス
 class Servo:
+    # GPIOポート番号
     PORT = 18
 
+    # コンストラクタ
     def __init__(self):
         GPIO.setup(PORT, GPIO.OUT)
 
         self.output = GPIO.PWM(PORT, 50)
         self.output.start(0.0)
 
+    # サーボを動かす
     def action(self):
         for dc in range(2, 12, 1):
             self.output.ChangeDutyCycle(dc)
@@ -22,5 +26,6 @@ class Servo:
             self.output.ChangeDutyCycle(dc)
             time.sleep(0.05)
 
+    # デストラクタ
     def __del__(self):
         self.output.stop()
--- engine/water_sensor.py
+++ engine/water_sensor.py
@@ -3,14 +3,19 @@
 
 import RPi.GPIO as GPIO
 
+# 水センサ制御用クラス
 class WaterSensor:
+    # GPIOポート番号
     PORT = 21
 
+    # コンストラクタ
     def __init__(self):
         GPIO.setup(PORT, GPIO.IN)
 
+    # センサーが水に濡れているかどうかを調べる
     def is_wet(self):
         return GPIO.input(PORT) == 0
 
+    # デストラクタ
     def __del__(self):
         pass
