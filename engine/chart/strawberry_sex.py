dce64fbc5d4ef287aae679e6f6e218e849f868a4
kadoppe <kadoppe@me.com>
2015-12-27 16:33:48 +0900

    曲完成

--- engine/chart/strawberry_sex.py
+++ engine/chart/strawberry_sex.py
@@ -16,27 +16,31 @@ CHART = [
     (Scale.REST, 4),
 
     (Scale.AS1, 2),
-    (Scale.GS0, 1),
+    (Scale.G0, 1),
     (Scale.AS1, 2),
-    (Scale.AS1, 1),
+    (Scale.G0, 1),
     (Scale.AS1, 2),
     (Scale.GS0, 4),
     (Scale.REST, 4),
 
     (Scale.GS0, 2),
     (Scale.G0, 2),
+    (Scale.F0, 1),
     (Scale.G0, 2),
-    (Scale.G0, 1),
     (Scale.F0, 5),
     (Scale.F0, 1),
+    (Scale.F0, 1),
     (Scale.F0, 2),
-    (Scale.DS0, 3),
 
-    (Scale.D0, 1),
+    (Scale.DS0, 2),
+    (Scale.DS0, 1),
     (Scale.D0, 1),
     (Scale.DS0, 1),
     (Scale.F0, 2),
     (Scale.G0, 3),
+    (scale.f0, 2),
+    (scale.G0, 1),
+    (Scale.A1, 3),
 
     (Scale.REST, 4)
 
