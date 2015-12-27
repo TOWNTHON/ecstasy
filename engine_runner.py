d6c0683944d4ef6cfaa3df9a9154d44497ec2b48
kadoppe <kadoppe@me.com>
2015-12-27 10:14:08 +0900

    エンジン終了処理を追加

--- engine_runner.py
+++ engine_runner.py
@@ -3,5 +3,11 @@
 
 from engine.engine import Engine
 
+# エンジン終了処理
+def exit_handler(signal, frame):
+  print("\nExit")
+  sys.exit(0)
+signal.signal(signal.SIGINT, exit_handler)
+
 engine = Engine()
 engine.run()
